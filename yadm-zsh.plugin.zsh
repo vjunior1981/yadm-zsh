e979d47d8fdf18cea1b14f6e16b3ae1dc7b0acf5
Vitor Jr <vitorjr81@gmail.com>
2019-08-18 20:10:52 -0300

    my own yadm zsh plugin

--- yadm-zsh.plugin.zsh
+++ yadm-zsh.plugin.zsh
@@ -39,23 +39,26 @@ function yadm_add_submodule() {
     yadm submodule add "${url}" "${1}"
 }
 
-autoload -Uz add-zsh-hook
-add-zsh-hook precmd _prompt_yadm_status
-
 #
 # Aliases
 # (sorted alphabetically)
 #
-alias y=yadm
+
+alias y='yadm'
+
 alias ya='yadm add'
-alias yaa='yadm add -u'
-alias yap='yadm apply'
+alias yya='yadm_add_all'
+alias ys='yadm_add_submodule'
 alias yapa='yadm add --patch'
 alias yau='yadm add --update'
+alias yav='yadm add --verbose'
+alias yap='yadm apply'
+
 alias yb='yadm branch'
 alias yba='yadm branch -a'
 alias ybd='yadm branch -d'
 alias ybda='yadm branch --no-color --merged | command grep -vE "^(\*|\s*(master|develop|dev)\s*$)" | command xargs -n 1 yadm branch -d'
+alias ybD='yadm branch -D'
 alias ybl='yadm blame -b -w'
 alias ybnm='yadm branch --no-merged'
 alias ybr='yadm branch --remote'
@@ -64,84 +67,109 @@ alias ybsb='yadm bisect bad'
 alias ybsg='yadm bisect good'
 alias ybsr='yadm bisect reset'
 alias ybss='yadm bisect start'
+
 alias yc='yadm commit -v'
-alias 'yc!'='yadm commit -v --amend'
+alias yc!='yadm commit -v --amend'
+alias ycn!='yadm commit -v --no-edit --amend'
 alias yca='yadm commit -v -a'
-alias 'yca!'='yadm commit -v -a --amend'
+alias yca!='yadm commit -v -a --amend'
+alias ycan!='yadm commit -v -a --no-edit --amend'
+alias ycans!='yadm commit -v -a -s --no-edit --amend'
 alias ycam='yadm commit -a -m'
-alias 'ycan!'='yadm commit -v -a --no-edit --amend'
-alias 'ycans!'='yadm commit -v -a -s --no-edit --amend'
+alias ycsm='yadm commit -s -m'
 alias ycb='yadm checkout -b'
-alias ycd='yadm checkout develop'
 alias ycf='yadm config --list'
-alias ycl='yadm clone --recursive'
+alias ycl='yadm clone --recurse-submodules'
 alias yclean='yadm clean -fd'
+alias ypristine='yadm reset --hard && yadm clean -dfx'
 alias ycm='yadm checkout master'
-alias 'ycn!'='yadm commit -v --no-edit --amend'
+alias ycd='yadm checkout develop'
+alias ycmsg='yadm commit -m'
 alias yco='yadm checkout'
 alias ycount='yadm shortlog -sn'
 alias ycp='yadm cherry-pick'
 alias ycpa='yadm cherry-pick --abort'
 alias ycpc='yadm cherry-pick --continue'
 alias ycs='yadm commit -S'
-alias ycsm='yadm commit -s -m'
+
 alias yd='yadm diff'
 alias ydca='yadm diff --cached'
-alias ydct='yadm describe --tags `yadm rev-list --tags --max-count=1`'
 alias ydcw='yadm diff --cached --word-diff'
-alias ydiff='yadm diff --no-index'
+alias ydct='yadm describe --tags `yadm rev-list --tags --max-count=1`'
+alias yds='yadm diff --staged'
 alias ydt='yadm diff-tree --no-commit-id --name-only -r'
 alias ydw='yadm diff --word-diff'
+
 alias yf='yadm fetch'
 alias yfa='yadm fetch --all --prune'
 alias yfo='yadm fetch origin'
-alias yy='yadm gui citool'
-alias yya='yadm gui citool --amend'
-alias yypull='yadm pull'
-alias yypush='yadm push'
-alias yh='yadm browse'
+
+alias yg='yadm gui citool'
+alias yga='yadm gui citool --amend'
+
+alias ygpur='ygu'
+
+alias yypull='yadm pull origin "$(yadm_current_branch)"'
+
+alias yypush='yadm push origin "$(yadm_current_branch)"'
+
+alias yysup='yadm branch --set-upstream-to=origin/$(yadm_current_branch)'
+alias ypsup='yadm push --set-upstream origin $(yadm_current_branch)'
+
 alias yhh='yadm help'
+
 alias yignore='yadm update-index --assume-unchanged'
 alias yignored='yadm ls-files -v | grep "^[[:lower:]]"'
+alias yadm-svn-dcommit-push='yadm svn dcommit && yadm push yadmhub master:svntrunk'
+
 alias yk='\yadmk --all --branches'
 alias yke='\yadmk --all $(yadm log -g --pretty=%h)'
+
 alias yl='yadm pull'
 alias ylg='yadm log --stat'
+alias ylgp='yadm log --stat -p'
 alias ylgg='yadm log --graph'
 alias ylgga='yadm log --graph --decorate --all'
 alias ylgm='yadm log --graph --max-count=10'
-alias ylgp='yadm log --stat -p'
 alias ylo='yadm log --oneline --decorate'
+alias ylol="yadm log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+alias ylols="yadm log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --stat"
+alias ylod="yadm log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'"
+alias ylods="yadm log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --date=short"
+alias ylola="yadm log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --all"
 alias ylog='yadm log --oneline --decorate --graph'
 alias yloga='yadm log --oneline --decorate --graph --all'
-alias ylol='yadm log --graph --pretty='\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit'
-alias ylola='yadm log --graph --pretty='\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit --all'
-alias ylp=_yadm_log_prettily
-alias ylum='yadm pull upstream master'
+alias ylp="_yadm_log_prettily"
+
 alias ym='yadm merge'
-alias yma='yadm merge --abort'
 alias ymom='yadm merge origin/master'
 alias ymt='yadm mergetool --no-prompt'
 alias ymtvim='yadm mergetool --no-prompt --tool=vimdiff'
 alias ymum='yadm merge upstream/master'
+alias yma='yadm merge --abort'
+
 alias yp='yadm push'
 alias ypd='yadm push --dry-run'
+alias ypf='yadm push --force-with-lease'
+alias ypf!='yadm push --force'
 alias ypoat='yadm push origin --all && yadm push origin --tags'
-alias ypristine='yadm reset --hard && yadm clean -dfx'
-alias ypsup='yadm push --set-upstream origin $(yadm_current_branch)'
 alias ypu='yadm push upstream'
 alias ypv='yadm push -v'
+
 alias yr='yadm remote'
 alias yra='yadm remote add'
 alias yrb='yadm rebase'
 alias yrba='yadm rebase --abort'
 alias yrbc='yadm rebase --continue'
+alias yrbd='yadm rebase develop'
 alias yrbi='yadm rebase -i'
 alias yrbm='yadm rebase master'
 alias yrbs='yadm rebase --skip'
-alias yrep='grep  --color=auto --exclude-dir={.bzr,CVS,.yadm,.hg,.svn}'
-alias yrh='yadm reset HEAD'
-alias yrhh='yadm reset HEAD --hard'
+alias yrh='yadm reset'
+alias yrhh='yadm reset --hard'
+alias yrm='yadm rm'
+alias yrmc='yadm rm --cached'
+alias yrmf='yadm rm --cached -f'
 alias yrmv='yadm remote rename'
 alias yrrm='yadm remote remove'
 alias yrset='yadm remote set-url'
@@ -149,9 +177,10 @@ alias yrt='cd $(yadm rev-parse --show-toplevel || echo ".")'
 alias yru='yadm reset --'
 alias yrup='yadm remote update'
 alias yrv='yadm remote -v'
+
 alias ysb='yadm status -sb'
 alias ysd='yadm svn dcommit'
-alias yi='yadm init'
+alias ysh='yadm show'
 alias ysi='yadm submodule init'
 alias ysps='yadm show --pretty=short --show-signature'
 alias ysr='yadm svn rebase'
@@ -164,12 +193,19 @@ alias ystd='yadm stash drop'
 alias ystl='yadm stash list'
 alias ystp='yadm stash pop'
 alias ysts='yadm stash show --text'
+alias ystall='yadm stash --all'
 alias ysu='yadm submodule update'
+
 alias yts='yadm tag -s'
 alias ytv='yadm tag | sort -V'
+
 alias yunignore='yadm update-index --no-assume-unchanged'
 alias yunwip='yadm log -n 1 | grep -q -c "\-\-wip\-\-" && yadm reset HEAD~1'
 alias yup='yadm pull --rebase'
 alias yupv='yadm pull --rebase -v'
+alias yupa='yadm pull --rebase --autostash'
+alias yupav='yadm pull --rebase --autostash -v'
+alias ylum='yadm pull upstream master'
+
 alias ywch='yadm whatchanged -p --abbrev-commit --pretty=medium'
 alias ywip='yadm add -A; yadm rm $(yadm ls-files --deleted) 2> /dev/null; yadm commit --no-verify -m "--wip-- [skip ci]"'
